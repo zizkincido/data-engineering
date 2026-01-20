fd74c85322a97782899c5957aa5aa908e03ec92c
Aziz Doğay Ekinci <49366820+zizkincido@users.noreply.github.com>
2026-01-20 14:16:42 +0000

    pipeline

---
+++ Postgres,Docker,Workflows/pipeline/pipeline.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pd
+print(f"Arguments {sys.argv}")
+month = int(sys.argv[1])
+df = pd.DataFrame({'day': [1,2], 'num_passengers': [3,4]})
+df['month'] = month
+print(df.head())
+df.to_parquet(f"output_{month}.parquet")
+print(f'hello pipeline, month={month}')
