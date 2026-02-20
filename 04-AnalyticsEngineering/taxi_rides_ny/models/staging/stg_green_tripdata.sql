18299dc0fd0a9affad2591c7e60377a1212f648b
Aziz Doğay Ekinci <dogay_ekinci@hotmail.com>
2026-02-20 18:22:35 +0000

    patch

--- 04-AnalyticsEngineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-AnalyticsEngineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,37 +1,38 @@
-with source as (
+{{ config(materialized='view') }}
 
-    select * from {{ source('staging', 'green_tripdata') }}
-
-),
-
-renamed as (
-
-    select
-        unique_row_id,
-        filename,
-        vendorid,
-        lpep_pickup_datetime,
-        lpep_dropoff_datetime,
-        store_and_fwd_flag,
-        ratecodeid,
-        pulocationid,
-        dolocationid,
-        passenger_count,
-        trip_distance,
-        fare_amount,
-        extra,
-        mta_tax,
-        tip_amount,
-        tolls_amount,
-        ehail_fee,
-        improvement_surcharge,
-        total_amount,
-        payment_type,
-        {{ get_payment_type_description(payment_type) }} as payment_type_descripted,
-        trip_type,
-        congestion_surcharge
-    from source
-
+with tripdata as
+(
+    select *,
+        row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
+    from {{ source('staging', 'green_tripdata') }}
+    where vendorid is not null
 )
 
-select * from renamed
+select
+--identifiers
+    {{dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
+    cast(vendorid as integer) as vendorid,
+    cast(ratecodeid as integer) as ratecodeid,
+    cast(pulocationid as integer) as pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+--timestamps
+    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+--trip info
+    store_and_fwd_flag,
+    cast(passenger_count as integer) as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    cast(trip_type as integer) as trip_type,
+--payment info
+    cast(fare_amount as numeric) as fare_amount
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(ehail_fee as numeric) as ehail_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    coalesce(cast(payment_type as integer),0) as payment_type,
+    {{ dbt_nytaxi.get_payment_type_description('payment_type') }} as payment_type_description
+from tripdata
+where rn=1
