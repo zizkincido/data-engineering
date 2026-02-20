5fbda2769ac270944421e91eb95b9ab840a3d202
Aziz Doğay Ekinci <dogay_ekinci@hotmail.com>
2026-02-20 19:06:28 +0000

    stg_yellow added

--- 04-AnalyticsEngineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-AnalyticsEngineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -1,43 +1,46 @@
-{{ config(materialized='view') }}
-
+{{
+    config(
+        materialized='table'
+    )
+}}
 with tripdata as
 (
     select *,
-        row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
-    from {{ source('staging', 'green_tripdata') }}
+        row_number() over(partition by vendorid, tpep_pickup,datetime) as rn
+    from {{ source('staging', 'yellow_tripdata') }}
     where vendorid is not null
 )
-
 select
---identifiers
-    {{dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
+-- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
 --timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
 --trip info
     store_and_fwd_flag,
     cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    cast(trip_type as integer) as trip_type,
+--yellow cabs are always street-hail
+    1 as trip_type,
 --payment info
     cast(fare_amount as numeric) as fare_amount,
     cast(extra as numeric) as extra,
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
-    coalesce(cast(payment_type as integer),0) as payment_type,
-    {{ dbt_nytaxi.get_payment_type_description('payment_type') }} as payment_type_description
+    coalesce(cast(payment_type as integer), 0) as payment_type,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description
 from tripdata
 where rn=1
 
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+-- dbt build --select <model.sql> --vars '{'is_test_run: false'}'
 {%if var('is_test_run', default=true)%}
-    limit 100
+limit 100
 {%endif%}
